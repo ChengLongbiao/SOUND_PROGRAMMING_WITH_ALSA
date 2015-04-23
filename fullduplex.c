d8e9a0aa6f07c6ae83a8c619de17a8ff00712634
Masinde Mtesigwa <masinde.masinde@metropolia.fi>
2015-04-23 10:57:32 +0300

    pitch shift

--- fullduplex.c
+++ fullduplex.c
@@ -1,21 +1,26 @@
 /* Use the newer ALSA API */
 #define ALSA_PCM_NEW_HW_PARAMS_API
 #include <stdio.h> 
-#include <stdlib.h> 
+#include <stdlib.h>
+#include <math.h> 
 #include <errno.h>
 #include <poll.h>
 #include <alsa/asoundlib.h>
 
 
+
+void smbPitchShift(float pitchShift, long numSampsToProcess, long fftFrameSize, long osamp, float sampleRate, float *indata, float *outdata);
+
+
 int main() {
 /*you locate these in a heap */
 	int 	restarting;
-	int		nchannels = 4;
+	int		nchannels = 2;
 	int		buffer_size = 512;
 	int 	sample_rate = 48000;
 	int 	bits = 16;
-  int 	err;
-  int frame_size;
+        int 	err;
+  	int frame_size;
 
 	char		*snd_device_in = "plughw:2,0";
 	char 		*snd_device_out = "plughw:2,0";
@@ -25,19 +30,19 @@ int main() {
 	//Set the device for capture and playback
 
       if ((err = snd_pcm_open(&playback_handle, snd_device_out, SND_PCM_STREAM_PLAYBACK, 0)) < 0) {
-          fprintf(stderr, "cannot open input audio device %s: %s\n", snd_device_in, snd_strerror(err));
+         	 fprintf(stderr, "cannot open input audio device %s: %s\n", snd_device_in, snd_strerror(err));
          
     	 exit(1);
       }
 
      if ((err = snd_pcm_open(&capture_handle, snd_device_in, SND_PCM_STREAM_CAPTURE, 0)) < 0) {
-          fprintf(stderr, "cannot open input audio device %s: %s\n", snd_device_out, snd_strerror(err));
+        	  fprintf(stderr, "cannot open input audio device %s: %s\n", snd_device_out, snd_strerror(err));
         
     	exit(1);
      } 
 
  int configure_alsa_audio(snd_device_in,  nchannels);
-int configure_alsa_audio(snd_device_out, nchannels);
+ int configure_alsa_audio(snd_device_out, nchannels);
 
 	restarting = 1;
 
@@ -53,28 +58,28 @@ int configure_alsa_audio(snd_pcm_t *device, int channels){
 
  /*alocate memory for hardware parameter structure*/
   	if ((err = snd_hw_params_malloc(&hw_params)) < 0) {
-    	 fprintf(stderr, "cannot allocate parameter structure (%s)\n", snd_strerror(err));
+ 	   	 fprintf(stderr, "cannot allocate parameter structure (%s)\n", snd_strerror(err));
     
      return 1;
    }
 
   //Fill structure from current audio parameters
  	if ((err = snd_pcm_hw_params_any(device, hw_params)) < 0){
-    	fprintf(stderr, "can not initialize parameter structure (%s)\n", snd_strerror(err));
+    		fprintf(stderr, "can not initialize parameter structure (%s)\n", snd_strerror(err));
     
-    return 1;
+          return 1;
  	}
 
  //set access type, sample rate sample format, channels
  	if ((err = snd_pcm_hw_params_set_access(device, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) {
    		 fprintf(stderr, "can not set access type: (%s)\n", snd_strerror(err));
   
-    	return 1;
+    	  return 1;
    	}
 
  	//bits = 16
   	if ((err = snd_pcm_hw_params_set_format(device, hw_params, SND_PCM_FORMAT_S16_LE)) < 0) {
-     	fprintf (stderr, "cannot set sample format: (%s)\n", snd_strerror(err));
+     		fprintf (stderr, "cannot set sample format: (%s)\n", snd_strerror(err));
      
      return 1;
     }
@@ -83,7 +88,7 @@ int configure_alsa_audio(snd_pcm_t *device, int channels){
 	if ((err = snd_pcm_hw_params_set_rate_near(device, hw_params, &tmp, 0)) < 0) {
    			fprintf(stderr, "cannot set sample rate: (%s)\n", snd_strerror(err));
    
-   		return 1;
+   	 return 1;
  	}
 
  	if (tmp != sample_rate) {
@@ -130,23 +135,25 @@ int configure_alsa_audio(snd_pcm_t *device, int channels){
 // preocess the data
 
 	int  inframes, outframes;
-  char *rdbuf;
-  int i;
+  	char *rdbuf;
+  	int i;
   rdbuf = (char *)malloc(frame_size);
+	long semitones = 3; // shift up by 3 semitones
+	float pitchShift = pow(2., semitones/12.);// convert semitones to factor
 
     	while (1) {
         		frame_size = channels * (bits / 8);
         		frames = buffer_size / frame_size;
 
         		if (restarting) {
-            		restarting = 0;
-              		/* drop any output we might got and stop */
-              		snd_pcm_drop(capture_handle);
-              		snd_pcm_drop(playback_handle);
+            			restarting = 0;
+        	      		/* drop any output we might got and stop */
+              			snd_pcm_drop(capture_handle);
+              			snd_pcm_drop(playback_handle);
             
         			     /* prepare for use */
-                	snd_pcm_prepare(capture_handle);
-               		snd_pcm_prepare(playback_handle);
+                		snd_pcm_prepare(capture_handle);
+               			snd_pcm_prepare(playback_handle);
 
           			 /* fill the whole output buffer */
           			 for (i = 0; i < fragments; i += 1)
@@ -154,33 +161,36 @@ int configure_alsa_audio(snd_pcm_t *device, int channels){
         		}
 
         		while ((inframes = snd_pcm_readi(capture_handle, rdbuf, frames)) < 0) {
-            		if (inframes == -EAGAIN)
+            			if (inframes == -EAGAIN)
                 			continue;
         			// by the way, writing to terminal emulator is costly if you use
         			// bad emulators like gnome-terminal, so don't do this.
-            		fprintf(stderr, "Input buffer overrun\n");
-            		restarting = 1;
-            		snd_pcm_prepare(capture_handle);
-       		 }
-        		if (inframes != frames)
-            		fprintf(stderr, "Short read from capture device: %d, expecting %d\n", inframes, frames);
+            			fprintf(stderr, "Input buffer overrun\n");
+            			restarting = 1;
+            			snd_pcm_prepare(capture_handle);
+       	       	 	}
+        			if (inframes != frames)
+            				fprintf(stderr, "Short read from capture device: %d, expecting %d\n", inframes, frames);
 
-        			/* now processes the frames */
+        				/* now processes the frames */
+			
         				do_something(rdbuf, inframes);
-
+					/*call smbPitchshift*/
+					smbPitchShift(pitchShift, bufferLengthFrames, 2048, 4, mAiffGetSampleRate(inFileName), data[0], data[0]);
+	
         				while ((outframes = snd_pcm_writei(playback_handle, rdbuf, inframes)) < 0) {
-            				if (outframes == -EAGAIN)
+            					if (outframes == -EAGAIN)
                 					continue;
             					fprintf(stderr, "Output buffer underrun\n");
             		      
-                      restarting = 1;
+                      				restarting = 1;
             					
-                      snd_pcm_prepare(playback_handle);
+                			      snd_pcm_prepare(playback_handle);
         				}
-      		  if (outframes != inframes)
-            fprintf(stderr, "Short write to playback device: %d, expecting %d\n", outframes, frames);
+      			  	if (outframes != inframes)
+           				 fprintf(stderr, "Short write to playback device: %d, expecting %d\n", outframes, frames);
     	
-      }
+      	}
       
       free(rdbuf);
 }
