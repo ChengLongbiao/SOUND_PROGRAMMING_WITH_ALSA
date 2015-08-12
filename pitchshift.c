a661d28a86e6ce4397d6038aa46a7d1bcadd90ea
Masinde Mtesigwa <masinde.masinde@metropolia.fi>
2015-08-12 20:22:21 +0300

    Pitch shifting

--- pitchshift.c
+++ pitchshift.c
@@ -38,7 +38,7 @@ void smbFft(float *fftBuffer, long fftFrameSize, long sign);
 //double smbAtan2(double x, double y);
 
 
-// -----------------------------------------------------------------------------------------------------------------
+
 
 
 void smbPitchShift(float pitchShift, long numSampsToProcess, long fftFrameSize, long osamp, float sampleRate, float *indata, float *outdata)
